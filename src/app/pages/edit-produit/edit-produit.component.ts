77cf6b7f43cb18e6f29ceefa2390f28606c4cf60
Lucas <lucasjaskowiak@yahoo.fr>
2025-06-25 11:39:35 +0200

    MAJ cours 2025 06 25

--- src/app/pages/edit-produit/edit-produit.component.ts
+++ src/app/pages/edit-produit/edit-produit.component.ts
@@ -10,7 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NotificationService } from '../../services/notification.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-produit',
@@ -22,27 +22,69 @@ export class EditProduitComponent {
   formBuilder = inject(FormBuilder);
   notification = inject(NotificationService);
   router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
   http = inject(HttpClient);
+
   formulaire = this.formBuilder.group({
-    nom: ['', [Validators.required, Validators.maxLength(20)]],
+    name: ['', [Validators.required, Validators.maxLength(20)]],
     description: ['', [Validators.maxLength(50)]],
   });
 
+  produitEditer: any;
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((parameters) => {
+      if (parameters['id']) {
+        this.http
+          .get('http://localhost:5000/product/' + parameters['id'])
+          .subscribe((produit) => {
+            this.formulaire.patchValue(produit);
+            this.produitEditer = produit;
+          });
+      }
+    });
+  }
+
   onAddProduct() {
     if (this.formulaire.valid) {
-      this.http
-        .post('http://localhost:5000/product', this.formulaire.value)
-        .subscribe({
-          next: (res) => {
-            this.notification.show('Le produit a bien été ajouté ! ', 'valid');
-            this.router.navigateByUrl('/accueil');
-          },
-          error: (err) => {
-            if (err.status === 409) {
-              this.notification.show('Un produit porte déjà ce nom', 'error');
-            }
-          },
-        });
+      if (this.produitEditer) {
+        this.http
+          .put(
+            'http://localhost:5000/product/' + this.produitEditer.id,
+            this.formulaire.value
+          )
+          .subscribe({
+            next: (res) => {
+              this.notification.show(
+                'Le produit a bien été modifié ! ',
+                'valid'
+              );
+              this.router.navigateByUrl('/accueil');
+            },
+            error: (err) => {
+              if (err.status === 409) {
+                this.notification.show('Un produit porte déjà ce nom', 'error');
+              }
+            },
+          });
+      } else {
+        this.http
+          .post('http://localhost:5000/product', this.formulaire.value)
+          .subscribe({
+            next: (res) => {
+              this.notification.show(
+                'Le produit a bien été ajouté ! ',
+                'valid'
+              );
+              this.router.navigateByUrl('/accueil');
+            },
+            error: (err) => {
+              if (err.status === 409) {
+                this.notification.show('Un produit porte déjà ce nom', 'error');
+              }
+            },
+          });
+      }
     }
   }
 }
